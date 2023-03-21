6c1bc8546caa05a2d5182136364d83fbcef9a39e
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-03-21 17:13:41 +0300

    Final update before merge

--- React/Homeworks/to-do-list/src/components/toDo/footer/index.js
+++ React/Homeworks/to-do-list/src/components/toDo/footer/index.js
@@ -1,51 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./footer.css"
 function Footer(props) {
-    const incompleteItemCount = props.items.filter(
-        (item) => !item.completed
-        ).length;
-        
-        function clearCompleted() {
-            props.setItems((prevState) => prevState.filter((item) => !item.completed));
-        }
-        
-        function handleClick(status) {
-            document.querySelectorAll(".itemContainer").forEach((item) => {
-            if (status === "All") item.style.display = "flex";
-            else if (status === "Active")
-                item.classList.contains("checkedItems")
-                ? (item.style.display = "none")
-                : (item.style.display = "flex");
-            else if (status === "Completed")
-                item.classList.contains("uncheckedItems")
-                ? (item.style.display = "none")
-                : (item.style.display = "flex");
-            });
-        }
+    
+    const uncompleteItemCount = props.items.filter(
+        (item) => !item.completed)
+        .length;
+
+    const completedItemCount = props.items.filter(
+        (item) => item.completed)
+        .length;
+    const [itemAmount, setItemAmount] = useState()
+    const [itemMessage, setItemMessage] = useState("")
+    
+    function clearCompleted() {
+        props.setItems((prevState) => prevState.filter((item) => !item.completed));
+    }
+    
+    function handleClick(status) {
+        document.querySelectorAll(".itemContainer").forEach((item) => {
+        if (status === "All") item.style.display = "flex";
+        else if (status === "Active")
+            item.classList.contains("checkedItems")
+            ? (item.style.display = "none")
+            : (item.style.display = "flex");
+        else if (status === "Completed")
+            item.classList.contains("uncheckedItems")
+            ? (item.style.display = "none")
+            : (item.style.display = "flex");
+        });
+    }
+    
     
     return (
         <div>
             <div className="footerContainer">
         <ul>
-            <li>{incompleteItemCount} items left</li>
+            <li>{itemAmount} {itemMessage}</li>
             <li>
             <a
                 href="#/"
-                onClick={() => handleClick("All")}
+                onClick={() => {
+                    setItemAmount(props.items.length)
+                    setItemMessage("uncompleted and completed")
+                    return (
+                        handleClick("All")
+                        
+                        )}}
                 className="footerButton"
             >
                 All
             </a>
             <a
                 href="#/Active"
-                onClick={() => handleClick("Active")}
+                onClick={() => {
+                    setItemAmount(uncompleteItemCount)
+                    setItemMessage("uncompleted")
+                    return(
+                    handleClick("Active")
+                    )}}
                 className="footerButton"
             >
                 Active
             </a>
             <a
                 href="#/Completed"
-                onClick={() => handleClick("Completed")}
+                onClick={() => {
+                    setItemMessage("completed")
+                    setItemAmount(completedItemCount)
+                    return( 
+                    handleClick("Completed")
+                    
+                    )}}
                 className="footerButton"
             >
                 Completed
@@ -54,7 +79,13 @@ function Footer(props) {
             <li
             id="clearCompleted"
             className="clearCompleted"
-            onClick={clearCompleted}
+            onClick={() => {
+                setItemAmount(uncompleteItemCount)
+                    setItemMessage("uncompleted")
+                return (
+                    clearCompleted(),
+                    handleClick("All")
+                    )}}
             >
             Clear Completed
             </li>
