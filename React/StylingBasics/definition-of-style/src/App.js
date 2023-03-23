487c76c63d5ac69f53e94a20b8610605ed79bddc
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-03-23 15:54:26 +0300

    Define A style in React.

--- React/StylingBasics/definition-of-style/src/App.js
+++ React/StylingBasics/definition-of-style/src/App.js
@@ -9,14 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{color:"red", backgroundColor:"aliceblue", paddingTop:"50px"}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem beatae quisquam consectetur, minima maxime quis debitis impedit architecto iusto mollitia asperiores, quo rerum. Repudiandae dolore consequuntur ab quo est.
+        </div>
+        <div className="card" style={{width: "18rem",margin:"50px", border:"7px", borderColor:"white", borderStyle:"solid", padding:"20px"}}>
+        <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
       </header>
     </div>
   );
