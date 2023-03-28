5aaad7367ee3834ccc33c7ac688cf21e59eddbc7
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-03-28 17:53:53 +0300

    First context trial created.

---
+++ React/context-api/src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import ThemeContext from '../context/ThemeContext';
+
+function Button() {
+    const data = useContext(ThemeContext);
+    console.log(data);
+    return (
+        <div>
+            Button ({ data})
+        </div>
+    )
+}
+
+export default Button
