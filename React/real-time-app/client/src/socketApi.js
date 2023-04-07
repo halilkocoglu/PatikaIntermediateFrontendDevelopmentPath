bfbc8e2d8fe1d0d263a889118a9c323fbf64c0fe
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-04-07 12:00:36 +0300

    Socket server connection

---
+++ React/real-time-app/client/src/socketApi.js
@@ -0,0 +1,12 @@
+import io from "socket.io-client"
+let socket;
+
+export const init = () => {
+    console.log("Connecting to server...");
+
+    socket = io("http://localhost:3001", {
+        transports: ["websocket"]
+    }) 
+    socket.on ("connect", () => 
+    console.log("Connected to server successfully"))
+}
