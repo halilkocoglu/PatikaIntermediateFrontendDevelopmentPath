0deebb66ae4209aae194c4cfaf7d9aca12be8720
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-05-16 17:26:21 +0300

    contact app end commit

--- React/contacts-app/src/components/Contacts/List/index.js
+++ React/contacts-app/src/components/Contacts/List/index.js
@@ -1,30 +1,32 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-function List({contacts}) {
-    const [filterText, setFilterText] = useState("");
-    const filtered = contacts.filter((contact) => {
-        return Object.keys(contact).some((key) => 
-            contact[key]
-            .toString()
-            .toLowerCase().includes(filterText.toLowerCase())
-        )
-    } )
-    // console.log(`Filtered: ${JSON.stringify(filtered)}`);
-    return (
-        <div>
-            <input placeholder='Filter Contact' value={filterText} onChange={(e) => setFilterText(e.target.value)}/>
-            <ul className='list'>
-                {filtered.map((contact,i) => (
-                    <li key={i}> 
-                        <span>{contact.fullname}</span>
-                        <span>{contact.phoneNumber}</span>
-                    </li>
-                ))}
-            </ul>
-            <div> Total Contacts ({filtered.length})  </div>
-            <br />
-        </div>
-    )
+function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+  const filtered = contacts.filter((contact) => {
+    return Object.keys(contact).some((key) =>
+      contact[key].toString().toLowerCase().includes(filterText.toLowerCase())
+    );
+  });
+  // console.log(`Filtered: ${JSON.stringify(filtered)}`);
+  return (
+    <div>
+      <input
+        placeholder="Filter Contact"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+      <ul className="list">
+        {filtered.map((contact, i) => (
+          <li key={i}>
+            <span>{contact.fullname}</span>
+            <span>{contact.phoneNumber}</span>
+          </li>
+        ))}
+      </ul>
+      <div> Total Contacts ({filtered.length}) </div>
+      <br />
+    </div>
+  );
 }
 
-export default List
+export default List;
